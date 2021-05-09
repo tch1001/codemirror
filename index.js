268f9bf4021a91057cdd95207c45460e0f45d43c
tch1001 <tanchienhao@gmail.com>
2021-05-09 23:08:48 +0800

    using ejs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
+
 app.get('/', (req,res) => {
-	res.send("hi")
+	res.render("index")
 })
 app.listen(port, () => {
 	console.log(`listening on ${port}`)
